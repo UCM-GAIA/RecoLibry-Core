07bb7989b1d1ff868f3f474f5eaf70d1e6f64089
jljorro <jljorro@ucm.es>
2018-12-10 12:06:05 +0100

    Completada la carga en tiempo de ejecución de la clase creada a partir de plantilla

--- RecoLibry/src/main/java/es/ucm/fdi/gaia/recolibry/RecommenderSystemConfiguration.java
+++ RecoLibry/src/main/java/es/ucm/fdi/gaia/recolibry/RecommenderSystemConfiguration.java
@@ -1,12 +1,8 @@
 package es.ucm.fdi.gaia.recolibry;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
 import com.google.inject.name.Names;
-
 import com.jiowa.codegen.JiowaCodeGeneratorEngine;
 import com.jiowa.codegen.config.JiowaCodeGenConfig;
 import es.ucm.fdi.gaia.codegen.tests.ClassGenerator;
@@ -16,7 +12,6 @@ import es.ucm.fdi.gaia.jcolibri.method.retrieve.NNretrieval.similarity.global.Av
 import es.ucm.fdi.gaia.recolibry.api.Query;
 import es.ucm.fdi.gaia.recolibry.api.RecommenderAlgorithm;
 import es.ucm.fdi.gaia.recolibry.examples.test1.GenresSimilarity;
-import es.ucm.fdi.gaia.recolibry.examples.test1.MovieCase;
 import es.ucm.fdi.gaia.recolibry.implementations.jcolibri.CSVConnector;
 import es.ucm.fdi.gaia.recolibry.implementations.jcolibri.LocalSimilarityConfiguration;
 import es.ucm.fdi.gaia.recolibry.implementations.jcolibri.QueryJColibri;
@@ -25,6 +20,11 @@ import es.ucm.fdi.gaia.recolibry.utils.BeansFactory;
 
 import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class to make the recommender configuration. It reads a JSON file
@@ -42,7 +42,7 @@ public class RecommenderSystemConfiguration extends AbstractModule {
         String[] attr2 = new String[] {"title", "String"};
         String[] attr3 = new String[] {"genres", "String[]"};
 
-        List<String[]> attributes = new ArrayList<String[]>();
+        List<String[]> attributes = new ArrayList<>();
         attributes.add(attr1);
         attributes.add(attr2);
         attributes.add(attr3);
@@ -57,7 +57,7 @@ public class RecommenderSystemConfiguration extends AbstractModule {
         JiowaCodeGeneratorEngine engine = new JiowaCodeGeneratorEngine(classGenerator);
         engine.start();
 
-        return System.getProperty("user.dir") + "/src/main/java/" + packageName.replace(".", "/") + "/" + "MovieCase2.java";
+        return System.getProperty("user.dir") + "/src/main/java/" + packageName.replace(".", "/") + "/MovieCase2.java";
     }
 
     private void compile(String... files) {
@@ -78,14 +78,18 @@ public class RecommenderSystemConfiguration extends AbstractModule {
             //TODO - Compilar dinamicamente MovieCase.java
             compile(path);
 
-            Thread.sleep(5000);
+            String classPath = System.getProperty("user.dir") + "/src/main/java/" + "es.ucm.fdi.gaia.recolibry.examples.test1.MovieCase2".replace(".", "/") + "/";
+            //Thread.sleep(5000);
 
             // Configuración del CSV Connector
-            Class clazz = Class.forName("es.ucm.fdi.gaia.recolibry.examples.test1.MovieCase2");
+            // Class clazz = Class.forName("es.ucm.fdi.gaia.recolibry.examples.test1.MovieCase2");
+            URL[] urlClass = new URL[]{new URL("file://" + classPath)};
+            URLClassLoader classLoader = new URLClassLoader(urlClass);
+            Class clazz = classLoader.loadClass("es.ucm.fdi.gaia.recolibry.examples.test1.MovieCase2");
             BeansFactory factory = new BeansFactory(clazz);
 
             bind(BeansFactory.class).annotatedWith(Names.named("beansFactory")).toInstance(factory);
-            bind(String.class).annotatedWith(Names.named("fileName")).toInstance("src/main/resources/movies.csv");
+            bind(String.class).annotatedWith(Names.named("fileName")).toInstance("./movies.csv");
             bind(Boolean.class).annotatedWith(Names.named("existTitleRow")).toInstance(true);
 
             // Make Local Similarity Function
@@ -101,7 +105,9 @@ public class RecommenderSystemConfiguration extends AbstractModule {
 
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
+        }/* catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/ catch (MalformedURLException e) {
             e.printStackTrace();
         }
 
