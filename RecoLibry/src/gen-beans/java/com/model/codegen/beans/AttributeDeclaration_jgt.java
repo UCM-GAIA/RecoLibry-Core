d75c656856f25c4246a16851a215b5a5d785d5c7
jljorro <jljorro@ucm.es>
2018-12-10 12:05:14 +0100

    Actualizadas las plantillas para jCOLIBRI

--- RecoLibry/src/gen-beans/java/com/model/codegen/beans/AttributeDeclaration_jgt.java
+++ RecoLibry/src/gen-beans/java/com/model/codegen/beans/AttributeDeclaration_jgt.java
@@ -18,7 +18,7 @@ import java.util.*;
  * AUTOMATICALLY GENERATED <br>
  *  
  * Template Bean: AttributeDeclaration_jgt <br>
- * Generation Timestamp: Wed Dec 05 17:21:52 CET 2018 <br>
+ * Generation Timestamp: Mon Dec 10 11:57:56 CET 2018 <br>
  * JIOWA CodeGen Version: jiowa-codegen-2.1.6 
  * 
  * <p>
