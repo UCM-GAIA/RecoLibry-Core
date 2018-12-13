0688ecf978e52c693c27d390f67af64d34968673
jljorro <jljorro@ucm.es>
2018-12-13 15:24:14 +0100

    Añadido un método en la interfaz Query para insertar un objeto bean

--- src/main/java/es/ucm/fdi/gaia/recolibry/implementations/jcolibri/QueryJColibri.java
+++ src/main/java/es/ucm/fdi/gaia/recolibry/implementations/jcolibri/QueryJColibri.java
@@ -1,8 +1,14 @@
 package es.ucm.fdi.gaia.recolibry.implementations.jcolibri;
 
 import es.ucm.fdi.gaia.jcolibri.cbrcore.CBRQuery;
+import es.ucm.fdi.gaia.jcolibri.cbrcore.CaseComponent;
 import es.ucm.fdi.gaia.recolibry.api.Query;
+import es.ucm.fdi.gaia.recolibry.utils.BeansFactory;
 
 public class QueryJColibri extends CBRQuery implements Query {
 
+    @Override
+    public void setBean(Object bean) {
+        this.setDescription((CaseComponent) bean);
+    }
 }
