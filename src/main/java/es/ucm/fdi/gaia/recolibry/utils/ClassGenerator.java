629f9e0a2d55d18c32ef6102a213ef35e236f476
jljorro <jljorro@ucm.es>
2018-12-13 14:43:56 +0100

    Modificado el class generator para añadir atributos que son de tipo List

--- src/main/java/es/ucm/fdi/gaia/recolibry/utils/ClassGenerator.java
+++ src/main/java/es/ucm/fdi/gaia/recolibry/utils/ClassGenerator.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.jiowa.codegen.config.JiowaCodeGenConfig;
 import com.jiowa.codegen.generator.AbstractGenerator;
+import com.model.codegen.beans.AttributeList_jgt;
 import com.model.codegen.beans.Attribute_jgt;
 import com.model.codegen.beans.CaseComponent_jCOLIBRI_jgt;
 
@@ -13,6 +14,7 @@ public class ClassGenerator extends AbstractGenerator{
 	private String className;
 	private String packageName;
 	private List<String[]> attributes; // Position 0 is name, position 1 is type
+	private List<String[]> attributesList;
 	
 	public ClassGenerator(JiowaCodeGenConfig config) {
 		super(config);
@@ -26,24 +28,42 @@ public class ClassGenerator extends AbstractGenerator{
 		cbr_class.setClassName(className);
 		
 		// Add attributes
-		List<Attribute_jgt> attributes_jgt = new ArrayList<Attribute_jgt>();
+		List<Attribute_jgt> attributes_jgt = new ArrayList<>();
 		
 		for(String[] a : attributes) {
 			Attribute_jgt attr = cbr_class.foreachAttribute.append_Attribute_jgt().setDataType(a[1]).setAttributeName(a[0]);
 			attributes_jgt.add(attr);
+		}
+
+		// Add attributes List
+		List<AttributeList_jgt> attributesList_jgt = new ArrayList<>();
+
+		for(String[] a : attributesList) {
+			AttributeList_jgt attr = cbr_class.foreachAttributeList.append_AttributeList_jgt().setDataType(a[1]).setAttributeName(a[0]);
+			attributesList_jgt.add(attr);
 		}
 		
 		// Constructor arguments
 		for(Attribute_jgt attr : attributes_jgt)
 			cbr_class.foreachAttribute.append_Argument_jgt(attr);
 
+		for(AttributeList_jgt attr : attributesList_jgt)
+			cbr_class.foreachAttributeList.append_ArgumentList_jgt(attr);
+
         // Constructor arguments init
         for(Attribute_jgt attr : attributes_jgt)
             cbr_class.foreachAttribute.append_AttributeInit_jgt(attr);
+
+		for(AttributeList_jgt attr : attributesList_jgt)
+			cbr_class.foreachAttributeList.append_AttributeListInit_jgt(attr);
 		
 		// Getters
 		for(Attribute_jgt attr : attributes_jgt)
 			cbr_class.foreachAttribute.append_Getter_jgt(attr);
+
+		// Getters List
+		for(AttributeList_jgt attr : attributesList_jgt)
+			cbr_class.foreachAttributeList.append_GetterList_jgt(attr);
 		
 		// Generate source file
 		updateSourceFile("java/" + cbr_class.getPackageName().replace('.', '/') + "/" + cbr_class.getClassName() + ".java", cbr_class);                        
@@ -62,5 +82,8 @@ public class ClassGenerator extends AbstractGenerator{
 	public void setAttributes(List<String[]> attributes) {
 		this.attributes = attributes;
 	}
-	
+
+	public void setAttributesList(List<String[]> attributesList) {
+		this.attributesList = attributesList;
+	}
 }
